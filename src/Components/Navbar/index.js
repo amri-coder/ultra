982ba355fbcc8e73a9a9709d4a17757937c123cc
amri debian <amri@debian>
2020-03-30 16:04:47 +0200

    Change navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,10 +4,24 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
+        
+            <div className="navbar">
+                <div className="container">
+                    <div className="logo">
+                        <h2 className="logo-text">Ultra Profile</h2>
+                    </div>
+                   
+                        <ul className="ul-list">
+                            <li className="list-item"><a href="#">Home</a></li>
+                            <li className="list-item"><a href="#">Work</a></li>
+                            <li className="list-item"><a href="#">Portfolio</a></li>
+                            <li className="list-item"><a href="#">Resume</a></li>
+                            <li className="list-item"><a href="#">About</a></li>
+                            <li className="list-item"><a href="#">Contact</a></li>
+                        </ul>
+                    
+                </div>
+            </div>
     );
 }
 

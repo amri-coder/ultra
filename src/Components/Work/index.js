b8c38d3e829ea10532236511816da04d8b25cb22
amri debian <amri@debian>
2020-03-31 17:55:23 +0200

    projet

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -1,9 +1,50 @@
 import React from "react";
+import axios from 'axios';
 
-const Work = () => {
-    return (<div>
-        Work
-    </div>);
+import { WorkSection,WorkTitle, Span, WorkPart, Icon, PartTitle, Line, PartDesc } from "./style";
+
+
+class Work extends React.Component { 
+
+    render() {
+        return (
+            <WorkSection>
+                <div className="container">
+       
+                    <WorkTitle>
+                        <Span>Mon</Span> Travail
+        </WorkTitle>
+       
+                    <WorkPart first="1">
+                        <Icon className="icon fa fa-chain fa-2x"></Icon>
+                        <PartTitle>Mobile Ux</PartTitle>
+                        <Line />
+                        <PartDesc>
+                            lefjdglkfdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdkfgerkgnroirgrtoj
+          </PartDesc>
+                    </WorkPart>
+
+                    <WorkPart>
+                        <Icon className="icon fa fa-chain fa-2x"></Icon>
+                        <PartTitle>Mobile Ux</PartTitle>
+                        <Line />
+                        <PartDesc>
+                            lefjdglkfdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdkfgerkgnroirgrtoj
+          </PartDesc>
+                    </WorkPart>
+
+                    <WorkPart>
+                        <Icon className="icon fa fa-chain fa-2x"></Icon>
+                        <PartTitle>Mobile Ux</PartTitle>
+                        <Line />
+                        <PartDesc>
+                            lefjdglkfdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdkfgerkgnroirgrtoj
+          </PartDesc>
+                    </WorkPart>
+                </div>
+            </WorkSection>
+        );
+    }
 };
 
 export default Work;

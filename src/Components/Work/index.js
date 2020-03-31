4d78cf98c9debf973b33ca31b93c2e769f0a2a89
amri debian <amri@debian>
2020-03-31 18:33:17 +0200

    changement dans le fichier Work

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -6,41 +6,41 @@ import { WorkSection,WorkTitle, Span, WorkPart, Icon, PartTitle, Line, PartDesc 
 
 class Work extends React.Component { 
 
+    state = {
+        works: []
+    }
+    componentDidMount() {
+        axios.get('js/data.json').then (res => {this.setState({works:res.data.works})})
+    }
+        
+    
     render() {
+
+        const { works } = this.state;
+        const worksList = works.map((workItem) => {
+            return (
+                    <WorkPart key={workItem.id}>
+                        <Icon className={workItem.icon_name}></Icon>
+                        <PartTitle>{workItem.title}</PartTitle>
+                        <Line />
+                        <PartDesc>
+                        {workItem.body}
+                        </PartDesc>
+                    </WorkPart>
+
+            );
+        }
+        );
+
         return (
             <WorkSection>
                 <div className="container">
        
                     <WorkTitle>
                         <Span>Mon</Span> Travail
-        </WorkTitle>
+                    </WorkTitle>
        
-                    <WorkPart first="1">
-                        <Icon className="icon fa fa-chain fa-2x"></Icon>
-                        <PartTitle>Mobile Ux</PartTitle>
-                        <Line />
-                        <PartDesc>
-                            lefjdglkfdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdkfgerkgnroirgrtoj
-          </PartDesc>
-                    </WorkPart>
-
-                    <WorkPart>
-                        <Icon className="icon fa fa-chain fa-2x"></Icon>
-                        <PartTitle>Mobile Ux</PartTitle>
-                        <Line />
-                        <PartDesc>
-                            lefjdglkfdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdkfgerkgnroirgrtoj
-          </PartDesc>
-                    </WorkPart>
-
-                    <WorkPart>
-                        <Icon className="icon fa fa-chain fa-2x"></Icon>
-                        <PartTitle>Mobile Ux</PartTitle>
-                        <Line />
-                        <PartDesc>
-                            lefjdglkfdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdkfgerkgnroirgrtoj
-          </PartDesc>
-                    </WorkPart>
+                  {worksList}
                 </div>
             </WorkSection>
         );

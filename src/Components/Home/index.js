e4b65fce0e17691cdd7b0f4c78c61470b873a277
amri debian <amri@debian>
2020-03-31 16:25:01 +0200

    projet

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import { HomeSection, HomeInformation, HomeTitle, HomeInfo, HomeDesc, HomeBtn, Span } from "./style";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <HomeSection>
+            <div className="container">
+                <HomeInformation>
+                    <HomeTitle>AMRI</HomeTitle>
+                    <HomeInfo>Directeur De Création</HomeInfo>
+                    <HomeDesc>
+                    Je suis développeur <Span>Back-End</Span> & <Span>Font-End</Span>, créer des conceptions modernes et réactives pour le Web et le mobile. travaillons ensemble. Je vous remercie.
+                    </HomeDesc>
+                    <HomeBtn>Let's begin</HomeBtn>
+                </HomeInformation>
+            </div>
+        </HomeSection>
     );
 }
 

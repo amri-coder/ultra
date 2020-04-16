5ddcef14cecabbe57ca22717295292c95b3f4bff
amri debian <amri@debian>
2020-04-16 16:11:56 +0200

    changement

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -3,8 +3,19 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className="creative">
+            <div className="container">
+                <div className="creative-info">
+                    <h2 className="info-title"><span>C'est</span> Moi</h2>
+                    <h4 className="info-dir">Directeur De Création</h4>
+                    <p className="info-desc">
+                    Je suis développeur web <a href="#">pour plus d'info</a> front & back
+                    </p>
+                    <p className="info-desc">
+                    
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,9 +1,73 @@
 import React from "react";
-
+import './style.css';
 
 const Profile = () => {
 
-    return (<div>Profile</div>);
+    return (
+        <div className="profile_skills">
+            <div className="container">
+                <div className="profile">
+                    <h2 className="profile-title"><span>Mon </span>Profil</h2>
+                    <ul className="profile-list">
+                        <li className="profile-item">
+                            <span>Nom</span>
+                            AMRI Maher
+                        </li>
+                        <li className="profile-item">
+                            <span>Date de naissance</span>
+                            **/**/****
+                        </li>
+                        <li className="profile-item">
+                            <span>Adresse</span>
+                            France
+                        </li>
+                        <li className="profile-item">
+                            <span>Tél</span>
+                            (0033)(0)6.52.96.02.44
+                        </li>
+                        <li className="profile-item">
+                            <span>Email</span>
+                            amri.maher@yahoo.com
+                        </li>
+                        <li className="profile-item">
+                            <span>Github</span>
+                            <span className="web">https://github.com/amri-coder</span>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div className="skills">
+                    <h2 className="skills-title">Quelques <span>compétences</span></h2>
+                    <p className="skills-desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos praesentium blanditiis esse cupiditate, omnis similique.
+                    </p>
+                    <div className="bar">
+                        <span className="title">Bootstrap</span>
+                        <span className="perc">100%</span>
+                        <div className="parent">
+                            <span className="sp1"></span>
+                        </div>
+                    </div>
+                    
+                    <div className="bar">
+                        <span className="title">CSS3</span>
+                        <span className="perc">90%</span>
+                        <div className="parent">
+                            <span className="sp2"></span>
+                        </div>
+                    </div>
+                    
+                    <div className="bar">
+                        <span className="title">Photoshop</span>
+                        <span className="perc">80%</span>
+                        <div className="parent">
+                            <span className="sp3"></span>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>);
 
 }
 
